3e960b2617f9093b93a8dc5a9cde2a1b6f6580b0
Krischal-001 <krischal001@gmail.com>
2025-09-05 07:22:45 +0545

    Code has Been Optimized Usind AI

--- Practice_Simple_Calculator_in_Python.py
+++ Practice_Simple_Calculator_in_Python.py
@@ -1,29 +1,35 @@
-operation =input("Enter the operation you want to perform (+, -, *, /): ")
-n=int(input("Enter the number of values you want to perform the operation on: "))   
-numbers=[]
+operation = input("Enter the operation you want to perform (+, -, *, /): ")
+if operation not in ['+', '-', '*', '/']:
+    print("Invalid operation selected.")
+    exit()
+
+n = int(input("Enter the number of values you want to perform the operation on: "))
+numbers = []
 for i in range(n):
-    num=float(input(f"Enter number{i+1} :"))
+    num = float(input(f"Enter number {i+1}: "))
     numbers.append(num)
-    print("You have entered:", numbers)
-if operation=="+":
-    result=sum(numbers)
+
+print("You have entered:", numbers)
+
+if operation == "+":
+    result = sum(numbers)
     print("The sum is:", result)
-elif operation=="-":
-    result=numbers[0]
+elif operation == "-":
+    result = numbers[0]
     for num in numbers[1:]:
-        result-=num
+        result -= num
     print("The difference is:", result)
-elif operation=="*":
-    result=1
+elif operation == "*":
+    result = 1
     for num in numbers:
-        result*=num
+        result *= num
     print("The product is:", result)
-elif operation=="/":
-    result=numbers[0]
+elif operation == "/":
+    result = numbers[0]
     for num in numbers[1:]:
-        if num==0:
+        if num == 0:
             print("Error: Division by zero is not allowed.")
             break
-        result/=num
+        result /= num
     else:
         print("The quotient is:", result)
